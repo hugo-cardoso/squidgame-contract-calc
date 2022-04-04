2caaf05f4d451606e33f2363a6ad3cdfb07bfd66
Hugo Cardoso <hugorafaelmc@gmail.com>
2022-04-04 13:32:57 -0300

    Added GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '../components/Analytics'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
 }
 
 export default MyApp
